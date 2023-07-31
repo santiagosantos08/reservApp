09609bcfa5b6aaf4fac917401062e4817c2936a4
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-31 23:55:47 +0000

    fixed tsconfig

--- reservApp/src/main.ts
+++ reservApp/src/main.ts
@@ -14,6 +14,21 @@ import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 
 import router from './router'
+/*
+//---firebase---
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyANiN8PTkZb7RRUqYYWLoncFO-8ZC0rdwk",
+  authDomain: "cecreservas-1237e.firebaseapp.com",
+  projectId: "cecreservas-1237e",
+  storageBucket: "cecreservas-1237e.appspot.com",
+  messagingSenderId: "852907542670",
+  appId: "1:852907542670:web:3c4112a1038c4025c7a1df",
+  measurementId: "G-PKFHWZKG09"
+};
+initializeApp(firebaseConfig);
+//---firebase---
+*/
 
 const app = createApp(App)
 
