633ddcdd0a21f2ecdf9b4e6a761fc6ab5e9d5069
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-31 08:06:22 +0000

    added draft for a login page

--- reservApp/src/App.vue
+++ reservApp/src/App.vue
@@ -1,11 +1,13 @@
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Comp1 from '@/components/Comp1.vue'
+  import Home from '@/views/Home.vue'
+</script>
+
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <Home />
     </v-main>
   </v-app>
 </template>
-
-<script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
--- reservApp/src/views/Home.vue
+++ reservApp/src/views/Home.vue
@@ -1,7 +1,8 @@
-<template>
-  <HelloWorld />
-</template>
-
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import Comp1 from '@/components/Comp1.vue'
 </script>
+
+<template>
+  <Comp1 />
+</template>
