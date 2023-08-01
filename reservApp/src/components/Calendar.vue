a165ea151fb7e88c566d033813e5e3d7e3e22c6a
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-08-01 07:03:21 +0000

    calendar w/o exporting date&time

--- reservApp/src/components/Calendar.vue
+++ reservApp/src/components/Calendar.vue
@@ -1,28 +1,42 @@
-<template>
-
-    <div class="center">
-      <DatePicker mode="dateTime" is24hr :time-accuracy='1' />
-    </div>
-
-</template>
-  
 <script lang="ts">
 import { ref } from 'vue';
-import { Calendar, DatePicker } from 'v-calendar';
+import { DatePicker } from 'v-calendar';
 import 'v-calendar/style.css';
 
 export default {
+  
   components: {
-    Calendar,
     DatePicker,
   },
   data() {
     return {
-      date: new Date(),
+      selected: new Date(),
+      //make the selected date available to outside components
     };
   },
+  watch: {
+    selected: function () {
+      console.log(`The selectedDate changed to`, this.selected);
+      //store the selected date and time from the calendar un a variable called reservation so then it can be logged and exported to outside components
+    },
+  },
+  getSelectedDate() {
+    return this.selected;//???!"#?!?"#!?!"#!"?#?!"#?!#?!"?#!"?#!"?#!"?#!"?
+  },
 }
+
+
 </script>
+
+<template>
+
+    <div class="center">
+      <DatePicker v-model="selected" mode="dateTime" is24hr :time-accuracy='1' :min-date="new Date()" />
+    </div>
+
+</template>
+  
+
 
 <style>
 .center {
--- reservApp/src/views/NewReservation.vue
+++ reservApp/src/views/NewReservation.vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup>
     import BottomNavBarVue from '@/components/BottomNavBar.vue';
     import calendar from '@/components/Calendar.vue';
+    import getSelectedDate from '@/components/Calendar.vue';
+    //store the selected date and time from the calendar un a variable called reservation so then it can be logged
+
+    let date = calendar.getSelectedDate();
+
+    function logDate() {
+        console.log(date);
+    }
+
 </script>
 
 <template>
-    <calendar></calendar>
+    <calendar />
+    <v-btn value="log" @click="logDate">log Date</v-btn>
     <BottomNavBarVue/>
 </template>
